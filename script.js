507ac13b2e8781fb5d6a232358f34ea49e4ec4ec
Utsavvv1 <143708173+Utsavvv1@users.noreply.github.com>
2024-12-21 22:50:39 +0530

    added eventlistener to make it functional

--- script.js
+++ script.js
@@ -4,5 +4,27 @@ for (let i = 0; i < 16*16 ; i += 1){
     const new_div = document.createElement('div');
     // new_div.textContent = i;
     new_div.className = "cell";
+
+    new_div.addEventListener('mouseenter', () => {
+        new_div.style.backgroundColor = getRandomColor();
+    });
+    // new_div.addEventListener('mouseleave', () => {
+    //     new_div.style.backgroundColor = 'white';
+    // });
     container.appendChild(new_div);
 }
+
+
+// const cell = document.querySelector('.cell');
+
+function getRandomColor(){
+    const letters = '0123456789ABCDEF';
+    let color = '#'
+    for (let i = 0; i < 6 ; i++){
+        color += letters[Math.floor(Math.random()*16)];
+
+    }
+    return color;
+}
+
+
