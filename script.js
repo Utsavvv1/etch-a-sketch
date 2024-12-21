fc9750446626a29095429b7bda9f145fc21eda0c
Utsavvv1 <143708173+Utsavvv1@users.noreply.github.com>
2024-12-21 23:22:04 +0530

    Added user input size of grid

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-const container = document.getElementById("container")
+const container = document.getElementById("container");
+const gridSize = document.getElementById('gridSize');
+const genGridbtn = document.getElementById('genGridbtn');
 
-for (let i = 0; i < 16*16 ; i += 1){
-    const new_div = document.createElement('div');
-    // new_div.textContent = i;
-    new_div.className = "cell";
+function createGrid(size) {
+    container.innerHTML = '';
 
-    new_div.addEventListener('mouseenter', () => {
-        new_div.style.backgroundColor = getRandomColor();
-    });
-    // new_div.addEventListener('mouseleave', () => {
-    //     new_div.style.backgroundColor = 'white';
-    // });
-    container.appendChild(new_div);
+    for (let i = 0; i < size * size; i++) {
+        const new_div = document.createElement('div');
+        new_div.className = "cell";
+        const flxbasis = `calc(100% / ${size})`;
+
+        new_div.style.flex = `1 0 ${flxbasis}`;
+
+        new_div.addEventListener('mouseenter', () => {
+            new_div.style.backgroundColor = getRandomColor();
+        });
+
+        container.appendChild(new_div);
+    }
 }
 
-
-// const cell = document.querySelector('.cell');
-
-function getRandomColor(){
+function getRandomColor() {
     const letters = '0123456789ABCDEF';
-    let color = '#'
-    for (let i = 0; i < 6 ; i++){
-        color += letters[Math.floor(Math.random()*16)];
-
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
 
+genGridbtn.addEventListener('click', () => {
+    const sizeGiven = parseInt(gridSize.value);
 
+    if (isNaN(sizeGiven) || sizeGiven < 1 || sizeGiven > 100) {
+        alert("Enter a valid grid size (1 <= size <= 100)");
+    } else {
+        createGrid(sizeGiven);
+    }
+});
+
+createGrid(16);
